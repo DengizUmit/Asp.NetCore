33f5fc19f3ede297790a9ff1975c1277ee8e1cc5
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-09 17:12:20 +0300

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate void MyDelegate(int number1, int number2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegateAddition = new MyDelegate(Addition);
+            myDelegateAddition.Invoke(1, 2);
+            MyDelegate myDelegateSubtraction = new MyDelegate(Subtraction);
+            myDelegateSubtraction.Invoke(3, 9);
+            MyDelegate myDelegateMultiplication = new MyDelegate(Multiplication);
+            myDelegateMultiplication.Invoke(9, 7);
+            MyDelegate myDelegateDivision = new MyDelegate(Division);
+            myDelegateDivision.Invoke(8, 4);
+
+            Console.WriteLine("\nBasic Math Calculator!");
+        }
+
+        static void Addition(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+        static void Subtraction(int number1, int number2)
+        {
+            Console.WriteLine(number1 - number2);
+        }
+        static void Multiplication(int number1, int number2)
+        {
+            Console.WriteLine(number1 * number2);
+        }
+        static void Division(int number1, int number2)
+        {
+            Console.WriteLine(number1 / number2);
+        }
+
+    }
+}
