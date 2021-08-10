4907fd915cf5a0f31c7ff0c3bc9f5d266cfbece3
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-10 13:55:34 +0300

    Middleware

---
+++ AspNetCore/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore
+{
+    public class RequestEditingMiddleware
+    {
+
+        private RequestDelegate _requestDelegete;
+
+        public RequestEditingMiddleware(RequestDelegate requestDelegete)
+        {
+            _requestDelegete = requestDelegete;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.ToString() == "/umit")
+                await context.Response.WriteAsync("Hiiiii Umit!!!");
+            else
+                await _requestDelegete.Invoke(context);
+        }
+    }
+}
