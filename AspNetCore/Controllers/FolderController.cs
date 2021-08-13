ec130a8a33e1a60f308c28f670901d6dbab0d3ea
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-13 17:44:00 +0300

    Folder -> List, Create, Remove

---
+++ AspNetCore/Controllers/FolderController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Controllers
+{
+    public class FolderController : Controller
+    {
+        public IActionResult List()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"));
+            var folders = directoryInfo.GetDirectories();
+
+            return View(folders);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string folderName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", folderName));
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(string folderName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", folderName));
+            if (directoryInfo.Exists)
+            {
+                directoryInfo.Delete(true);
+            }
+
+            return RedirectToAction("List");
+        }
+    }
+}
