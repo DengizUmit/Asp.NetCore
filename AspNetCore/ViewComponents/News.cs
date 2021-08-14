02b68ae1182bf84358155a1a152a37c9f18c8df4
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-14 12:42:50 +0300

    View Component

---
+++ AspNetCore/ViewComponents/News.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.ViewComponents
+{
+    public class News : ViewComponent
+    {
+        public IViewComponentResult Invoke( string color="default")
+        {
+            var list = NewsContext.List;
+            if (color == "default")
+            {
+                return View(list);
+            }
+            else if (color == "red")
+            {
+                return View("red", list);
+            }
+            else
+            {
+                return View("blue", list);
+            }
+        }
+    }
+}
