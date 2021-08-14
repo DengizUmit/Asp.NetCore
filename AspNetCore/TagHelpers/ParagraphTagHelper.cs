605542fd3b9aae86fb5bd3da3bf8cc0f27845a8a
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-14 13:43:35 +0300

    Custom TagHelpers

---
+++ AspNetCore/TagHelpers/ParagraphTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("paragraph")]
+    public class ParagraphTagHelper : TagHelper
+    {
+        public string ShortDescription { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"<b>Custom Tag Helper works => {ShortDescription}</b>");
+        }
+    }
+}
