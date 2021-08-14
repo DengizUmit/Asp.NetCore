c054dd1eeff61a9eee4526d5a822f6629c4d2ad2
Ümit Dengiz <umit.dengiz@enqura.com>
2021-08-14 12:12:19 +0300

    Partial View

---
+++ AspNetCore/Models/NewsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Models
+{
+    public class NewsContext
+    {
+        public static List<News> List = new()
+        {
+            new News { Title = "News 1" },
+            new News { Title = "News 2" }
+        };
+    }
+}
